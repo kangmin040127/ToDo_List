1bb2132941ff57154f8f29ee69d0a663ebb1a166
kangmin040127 <104123033+kangker127@users.noreply.github.com>
2022-06-02 15:51:20 +0900

    end of todoList

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
 let taskInput = document.getElementById('task-input');
 let plusBtn = document.getElementById('plus-btn');
+let underLine = document.getElementById('under-line');
+let tabs = document.querySelectorAll(".task-tabs div");
+let menuId = 'all';
 let taskList = [];
-plusBtn.addEventListener('click', addTask);
+let filterList = [];
+let doneList = [];
+plusBtn.addEventListener('mousedown', addTask);
+taskInput.addEventListener("keyup" ,function (event) {
+  if (event.keyCode === 13) {
+    addTask(event);
+  }
+});
+console.log(tabs);
+
+for(let i = 1; i<tabs.length; i++){
+  tabs[i].addEventListener('click', function(event){
+    filter(event)})
+}
+
+function filter(event){
+  filterList = [];
+  doneList = [];
+  menuId = event.target.id;
+  document.getElementById("under-line").style.width = event.target.offsetWidth + "px";
+  document.getElementById("under-line").style.top = event.target.offsetTop + event.target.offsetHeight + "px";
+  document.getElementById("under-line").style.left = event.target.offsetLeft + "px";
+  if(menuId == 'all'){
+    render();
+  } else if (menuId == 'ongoing'){
+      for(let i = 0; i<taskList.length; i++){
+        if(taskList[i].isComplete == false){
+          filterList.push(taskList[i]);
+        }
+      }
+      render();
+  } else if(menuId == 'done'){
+    for(let i =0; i< taskList.length; i++){
+      if(taskList[i].isComplete == true){
+        doneList.push(taskList[i])
+      }
+    }
+    render();
+  }
+}; 
 
 function addTask(){
   let task = {
@@ -16,23 +58,32 @@ function addTask(){
 
 function render(){
   let resultHTML = "";
-  for(let i = 0; i< taskList.length; i++){
-    if(taskList[i].isComplete == true){
+  let list = [];
+  if(menuId == 'all'){
+    list = taskList;
+  } else if(menuId == 'ongoing'){
+    list = filterList;
+  } else if(menuId == 'done'){
+    list = doneList;
+  }
+
+  for(let i = 0; i< list.length; i++){
+    if(list[i].isComplete){
       resultHTML += `
         <div class="task-done">
-          <div class="task-done-text">${taskList[i].taskContent}</div>
+          <div class="task-done-text">${list[i].taskContent}</div>
           <div class="task-done-btn">
-            <button class="check-btn" onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-arrow-rotate-left"></i></button>
-            <button class="cancel-btn" onclick="deleteTask('${taskList[i].id}')"><i class="fa-solid fa-trash-can"></i></button>
+            <button class="check-btn" onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-arrow-rotate-left"></i></button>
+            <button class="cancel-btn" onclick="deleteTask('${list[i].id}')"><i class="fa-solid fa-trash-can"></i></button>
           </div>
         </div>`
     } else{
         resultHTML += 
         `<div class="task">
-          <div class="task-text">${taskList[i].taskContent}</div>
+          <div class="task-text">${list[i].taskContent}</div>
           <div>
-            <button class="check-btn" onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-check"></i></button>
-            <button class="cancel-btn" onclick="deleteTask('${taskList[i].id}')"><i class="fa-solid fa-trash-can"></i></button>
+            <button class="check-btn" onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-check"></i></button>
+            <button class="cancel-btn" onclick="deleteTask('${list[i].id}')"><i class="fa-solid fa-trash-can"></i></button>
           </div>
         </div>`;
     }
@@ -57,8 +108,17 @@ function toggleComplete(id){
 function deleteTask(id){
   for(let i = 0; i < taskList.length; i++){
     if(taskList[i].id == id){
-      taskList.splice(i,1);
-      break;
+      if(menuId == 'all'){
+        taskList.splice(i,1);
+        break;
+      } else if(menuId == 'ongoing'){
+        console.log(filterList.indexOf(taskList[i]));
+        filterList.splice(filterList.indexOf(taskList[i]),1);
+        taskList.splice(i,1);
+      } else if(menuId == 'done'){
+        doneList.splice(doneList.indexOf(taskList[i]),1);
+        taskList.splice(i,1);
+      }
     }
   }
   console.log(taskList);
